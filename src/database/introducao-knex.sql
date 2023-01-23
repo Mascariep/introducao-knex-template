9f4ca7485cfa17f2159bd3286d377413373adcf3
Mascariep <edipohc@gmail.com>
2023-01-23 12:29:09 -0300

    Exercícios práticos

--- src/database/introducao-knex.sql
+++ src/database/introducao-knex.sql
@@ -1,3 +1,4 @@
+-- Active: 1674471887896@@127.0.0.1@3306
 
 -- Tabelas já foram criadas
 CREATE TABLE bands (
@@ -11,3 +12,21 @@ CREATE TABLE songs (
     band_id TEXT NOT NULL,
     FOREIGN KEY (band_id) REFERENCES bands (id)
 );
+
+---------------------------------------------------------
+SELECT * FROM bands; -- VISUALIZAR A TABELA bands
+--DROP TABLE bands; --DELETAR A TABELA bands
+
+SELECT * FROM songs; -- VISUALIZAR A TABELA songs
+--DROP TABLE songs; -- DELETAR A TABELA songs
+---------------------------------------------------------
+--INSERT INTO bands (id, name) -- POPULANDO A TABELA bands
+--VALUES
+    --("b001", "Slipknot");
+
+--UPDATE bands
+--SET
+    --id = "b002",
+    --name = "Korn"
+--WHERE
+    --id = "b001";
